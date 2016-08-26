aad41849e17fdccc3498651c8caf074ddd06f4ab
ophid1an <dermentzoglou.ioannis@gmail.com>
2016-08-26 15:10:26 +0300

    Second commit

--- index.js
+++ index.js
@@ -4,11 +4,27 @@ var port = process.env.PORT || 8000;
 var server = http.createServer(function (request, response) {
   if (request.method === 'GET') {
     var headers = request.headers;
-    var keys = Object.keys(headers);
+
+    /*var keys = Object.keys(headers);
     keys.forEach(function(key) {
       console.log(key + ': ' + '('+typeof headers[key]+') ' + headers[key]);
-    });
+    });*/
 
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'application/json');
+    // Note: the 2 lines above could be replaced with this next one:
+    // response.writeHead(200, {'Content-Type': 'application/json'})
+
+    var responseBody = {
+      ipaddress: headers['host'],
+      language: headers['accept-language'],
+      software: headers['user-agent']
+    };
+
+    response.write(JSON.stringify(responseBody));
+    response.end();
+    // Note: the 2 lines above could be replaced with this next one:
+    // response.end(JSON.stringify(responseBody))
   }
 
 
